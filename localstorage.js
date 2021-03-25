8a6f31a734bb715625fd802e37876d241a73f637
Wildprogrammingape <46387551+Wildprogrammingape@users.noreply.github.com>
2021-03-24 23:26:55 -0400

    Add files via upload

---
+++ localstorage.js
@@ -0,0 +1,22 @@
+// store data in local storage 存储数据
+// 存储在local storage的必须是string 的key-value组合，存储在browser中
+// localStorage.setItem('name','mario');
+// localStorage.setItem('age',50);
+
+// get data from local storage 获取数据
+// let name = localStorage.getItem('name');
+// let age = localStorage.getItem('age');
+// console.log(age);
+
+// update data 更新
+// localStorage.setItem('name','chunli');
+// name = localStorage.getItem('name');
+// console.log(name,age);
+
+// delete data 删除
+// localStorage.removeItem('name');
+// localStorage.clear(); // 删除所有local storage的数据
+
+// JSON.stringify() 用来convert Array of objects to JSON string
+// JSON.parse() 用来 convert JSON to Array
+
